74af8d3161a25b6ca08be7cc66c3548afcd282d6
ssankathali <sunitha.sankathali@gmail.com>
2020-12-15 08:07:27 -0500

    Tdf&Mdf

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Template driven forms';
+  contact: contact
+  countryList: country[] = [
+    new country("1", "India"),
+    new country("2", "USA"),
+    new country("3", "Canada")
+  ];
+
+  onSubmit() {
+    console.log(this.contact);
+  }
+
+  ngOnInit() {
+    this.contact = {
+      firstname: "Sachin",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Mumbai", street: "Perry Cross Rd", pincode: "400050" }
+    };
+
+  }
+  setDefaults() {
+    this.contact = {
+      firstname: "Sachin",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Mumbai", street: "Perry Cross Rd", pincode: "400050" }
+    };
+  }
+
+  changeCountry() {
+    this.contact.country = "1";
+  }
+
+  reset(contactForm: NgForm) {
+    contactForm.resetForm();
+  }
+}
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+}
+
+export class contact {
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  isMarried: boolean;
+  country: string;
+  address: {
+    city: string;
+    street: string;
+    pincode: string
+  }
+}
+
