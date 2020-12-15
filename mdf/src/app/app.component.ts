f8f976123ff8d8fc5a821856d7c25911b0cce68f
ssankathali <sunitha.sankathali@gmail.com>
2020-12-15 08:36:47 -0500

    made changes in reactive forms

--- mdf/src/app/app.component.ts
+++ mdf/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mdf';
+  countryList: country[] = [
+    new country("1", "India"),
+    new country("2", "USA"),
+    new country("3", "Canada")
+  ];
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode: new FormControl()
+    })
+  })
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
 }
